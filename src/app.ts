e3eeb7d9f6e6271f6cb122b3b18581b22e783c6f
Harysson <haryssonsoares@gmail.com>
2023-10-22 11:22:51 -0300

    chore: rollback

--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,20 @@
 import express from "express";
 import cron from "node-cron";
 import swaggerUi from "swagger-ui-express";
-import swaggerJSDoc from "swagger-jsdoc";
 
 import { connection } from "./db/conn";
 import { getDataFromSpaceX } from "./utils/getDataFromSpaceXAPI";
 import { router } from "./routes/routes";
 import { seed } from "./utils/seed";
 
-import { swaggerDef } from "./swaggerDef";
-
-const options = {
-  swaggerDef,
-  // Paths to files containing OpenAPI definitions
-  apis: ["./*.js", "./*.ts"],
-};
-
-const swaggerSpec = swaggerJSDoc(options);
+import swaggerDoc from "./swagger.json";
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.json());
 
-app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
 app.use("/", router);
 
