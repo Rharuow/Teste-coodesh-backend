dc955961ccfdf59de491a941a0da4e566f659653
Harysson <haryssonsoares@gmail.com>
2023-10-24 19:38:34 -0300

    chore: deploy

--- src/db/conn.ts
+++ src/db/conn.ts
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 
 async function connection() {
   try {
+    console.log("MONGODB_CONNECTION = ", process.env.MONGODB_CONNECTION);
     mongoose.set("strictQuery", true);
     await mongoose.connect(process.env.MONGODB_CONNECTION || "");
     console.log("conectado ao banco");
