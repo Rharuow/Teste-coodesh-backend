03afacd3cdce3c1a80a8360550d32ada761e9e69
Harysson <haryssonsoares@gmail.com>
2023-10-24 19:12:49 -0300

    refactor: generate colors function

--- src/utils/generateColor.ts
+++ src/utils/generateColor.ts
@@ -1,6 +1,27 @@
-import CompColors from "complementary-colors";
+interface RGB {
+  r: number;
+  g: number;
+  b: number;
+}
 
 export const handleColor = (color: string) => {
-  const newColor = new CompColors(color).square()[2];
-  return `rgb(${newColor.r}, ${newColor.g}, ${newColor.b})`;
+  const variant = Math.floor(Math.random() * 100 + 80);
+
+  const rgb = [
+    Number(color.split("(")[1].split(",")[0]),
+    Number(color.split("(")[1].split(",")[1]),
+    Number(color.split("(")[1].split(",")[2]),
+  ];
+
+  const [r, g, b] = rgb.map((c, index) =>
+    c <= 255 - variant
+      ? c + variant
+      : c >= variant
+      ? c - variant
+      : index % 2 === 0
+      ? variant + 50
+      : variant
+  );
+
+  return `rgb(${r}, ${g}, ${b})`;
 };
